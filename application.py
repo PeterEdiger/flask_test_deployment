16feac084d71110c97ca0ef92b19bfaeb7fc8d2a
peet <teilzeitjobbewerber@gmx.de>
2022-12-14 20:16:22 +0100

    first html

--- application.py
+++ application.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 application = Flask(__name__)
 
 
 @application.route('/')
 def hello_world():  # put application's code here
-    return "<h1> Wanjka du Saftsack </h1>"
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
